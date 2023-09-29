bd6a312ae82c89c7416a15b85ac1f8637d46f891
eudevnane <nanewjmorais@icloud.com>
2023-09-29 12:00:58 -0300

    ALteração do código

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-calcular(50,50)
 
-let saldoVitorias = 50
-let nivel = " "
+calcular();
+let saldoVitorias = 100
+let saldoDerrotas = 20
 
-function  calcular ()  {
+function  calcular (saldoVitorias , saldoDerrotas)  {
+            return saldoVitorias - saldoDerrotas
     
 }
 
+let nivel = " "
 let resultadoCalculo = saldoVitorias
 
 if (resultadoCalculo <= 10) {
     nivel = "Ferro"
 }
-else if (resultadoCalculo > 10 && resultadoCalculo <= 20) {
+else if (resultadoCalculo > 11 && resultadoCalculo <= 20) {
     nivel = "Bronze"
 }
-else if (resultadoCalculo > 20 && resultadoCalculo <= 50) {
+else if (resultadoCalculo > 21 && resultadoCalculo <= 50) {
     nivel = "Prata"
 }
-else if (resultadoCalculo > 50 && resultadoCalculo <= 80) {
+else if (resultadoCalculo > 51 && resultadoCalculo <= 80) {
     nivel = "Ouro"
 }
-else if (resultadoCalculo > 80 && resultadoCalculo <= 90) {
+else if (resultadoCalculo > 81 && resultadoCalculo <= 90) {
     nivel = "Diamante"
 }
-else if (resultadoCalculo > 90 && resultadoCalculo <= 100) {
+else if (resultadoCalculo > 91 && resultadoCalculo <= 100) {
     nivel = "Lendário"
 }
-else if (resultadoCalculo > 100) {
+else if (resultadoCalculo > 101) {
     nivel = "Imortal"
 }
 console.log("O Herói tem saldo de " + saldoVitorias + " vitórias e está no nível " + nivel)
